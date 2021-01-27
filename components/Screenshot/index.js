3c552f8aeaaac960dab0b18c36aa9823db1c39f4
Scott Adkin <scottadkin001@gmail.com>
2021-01-27 20:43:26 +0000

    Match screenshot now works on page load :)

--- components/Screenshot/index.js
+++ components/Screenshot/index.js
@@ -1,16 +1,180 @@
 import styles from './Screenshot.module.css'
+import {useEffect, useRef} from "react";
 
 //fix screenshot not loading on page back
 
+class MatchScreenshot{
+
+    constructor(canvas, image, map, players, teams, matchData){
+
+        console.log(`new match screenshot`);
+        
+        this.canvas = canvas;
+        this.context = this.canvas.getContext("2d");
+
+        this.map = map;
+        this.players = JSON.parse(players);
+        this.teams = parseInt(teams);
+
+        this.matchData = JSON.parse(matchData);
+
+        console.log(this.matchData);
+
+        this.image = new Image();
+        this.image.src = image;
+
+        this.colors = {
+            "red": "rgb(226,0,0)",
+            "blue": "rgb(62,144,194)",
+            "green": "rgb(0,181,0)",
+            "yellow": "rgb(255,255,0)"
+        };
+
+        this.teamPlayerCount = [0,0,0,0];
+
+        //this.scaleImage();
+
+        this.image.onload = () =>{
+         //   console.log(`image loaded`);
+            this.render();
+        }   
+
+
+    }
+
+    x(input){
+        return (this.canvas.width * 0.01) * input;
+    }
+
+    y(input){
+        return (this.canvas.height * 0.01) * input
+    }
+
+    getTeamColor(team){
+
+        switch(team){
+            case 0: {   return this.colors.red; }
+            case 1: {   return this.colors.blue; } 
+            case 2: {   return this.colors.green; } 
+            case 3: {   return this.colors.yellow; }
+            default: {  return "white; "} 
+        }
+    }
+
+    renderStandardTeamGamePlayer(c, team, name, score, bFinal){
+
+        const scoreOffset = this.x(30);
+        const rowHeight = this.y(3.2);
+        const index = this.teamPlayerCount[team];
+        const scoreFontSize = this.y(2.5);
+        const teamHeaderFontSize = this.y(2.9);
+        let maxPlayersPerTeam = 0;
+        
+        if(this.teams < 3){
+            maxPlayersPerTeam = 18;
+        }else{
+            maxPlayersPerTeam = 9;
+        }
+
+        
+
+        let startY = 0;
+        let startX = 0;
+        
+        switch(team){
+            case 0: {   startY = 20; startX = 10;} break;
+            case 1: {   startY = 20; startX = 55;} break;
+            case 2: {   startY = 60; startX = 10;} break;
+            case 3: {   startY = 60; startX = 55;} break;
+        }
+
+        const x = this.x(startX);
+        const y = this.y(startY) + (rowHeight * index);
+
+        c.fillStyle = this.getTeamColor(team);
+
+        if(bFinal !== undefined){
+
+            if(index <= maxPlayersPerTeam){
+                return;
+            }
+            c.font = this.y(1.8)+"px Arial";
+            c.fillText(`${index - maxPlayersPerTeam} Player[s] not shown`,x , this.y(startY) + (rowHeight * maxPlayersPerTeam));
+            return;
+        }
+
+        if(index >= maxPlayersPerTeam){
+            this.teamPlayerCount[team]++;
+            return;
+        }
+
+        //draw team headers
+        if(index === 0){
+
+            let teamTitle = "";
+
+            switch(team){
+                case 0: {  teamTitle = `Red Team`; } break;
+                case 1: {  teamTitle = `Blue Team`; } break;
+                case 2: {  teamTitle = `Green Team`; } break;
+                case 3: {  teamTitle = `Yellow Team`; } break;
+            }
+
+            c.font = teamHeaderFontSize+"px Arial";
+            c.fillText(teamTitle, x, y - this.y(3.5));
+            c.fillText(this.matchData[`team_score_${team}`], x + scoreOffset, y - this.y(3.5));
+
+        }
+
+
+        c.font = scoreFontSize+"px Arial";
+        c.fillText(name, x, y);
+        c.fillText(score,x + scoreOffset, y);
+
+        this.teamPlayerCount[team]++;
+    }
+
+    render(){
+
+        const c = this.context;
+
+        console.log("render");
+
+        c.drawImage(this.image, 0, 0, this.canvas.width, this.canvas.height);
+        c.fillStyle = "rgba(0,0,0,0.45)";
+        c.fillRect(0, 0, this.canvas.width, this.canvas.height);
+        let p = 0;
+
+        for(let i = 0; i < this.players.length; i++){
+
+            p = this.players[i];
+            //console.log(p);
+            this.renderStandardTeamGamePlayer(c, p.team, p.name, p.score);
+        }
+
+        for(let i = 0; i < this.teams; i++){
+            this.renderStandardTeamGamePlayer(c,i,'','',true);
+        }
+    }
+}
+
+
 
 const Screenshot = ({map, totalTeams, players, image, matchData}) =>{
+
+    const test = useRef(null);
+
+    useEffect(() =>{
+        new MatchScreenshot(test.current, image, map, players,totalTeams, matchData);
+    });
+
 
     return (<div className={`${styles.wrapper} center`}>
         <div className="default-header">
             Match Screenshot
         </div>
         <div className={`${styles.content} center`}>
-            <canvas id="m-sshot" className="match-screenshot center" 
+            <canvas ref={test} id="m-sshot" className="match-screenshot center" 
                 data-match-data={matchData} 
                 data-map={map} 
                 data-image={image}
@@ -18,8 +182,7 @@ const Screenshot = ({map, totalTeams, players, image, matchData}) =>{
                 data-players={players} 
                 width="1920" height="1080">
             </canvas>
-            <script dangerouslySetInnerHTML={{__html: `new MatchScreenshot("m-sshot", ${JSON.stringify(image)}, ${JSON.stringify(map)}, ${JSON.stringify(players)}, ${JSON.stringify(totalTeams)}, ${JSON.stringify(matchData)});`}}></script>
-        </div>
+                </div>
     </div>);
 }
 
