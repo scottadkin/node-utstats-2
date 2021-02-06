d4b76fd7704b0be56c6d4b53c28507936ade9a8e
Scott Adkin <scottadkin001@gmail.com>
2021-02-06 12:35:06 +0000

    Added ctf carry times and dropped times

--- components/MatchCTFCaps/index.js
+++ components/MatchCTFCaps/index.js
@@ -40,6 +40,9 @@ const MatchCTFCaps = ({players, caps, matchStart, totalTeams}) =>{
     let currentName = '';
     let coverElems = [];
     let assistElems = [];
+    let carryElems = [];
+    let totalCarryTime = 0;
+    let totalDropTime = 0;
 
     let bgColor = '';
     let currentCoverNames = 0;
@@ -56,10 +59,18 @@ const MatchCTFCaps = ({players, caps, matchStart, totalTeams}) =>{
         assistElems = [];
         covers = [];
         assists = [];
+        carryElems = [];
         currentCoverNames = new Map();
+        totalCarryTime = 0;
+        totalDropTime = 0;
 
         c.covers = c.covers.split(',');
         c.assists = c.assists.split(',');
+        c.assist_carry_times = c.assist_carry_times.split(',');
+        c.assist_carry_ids = c.assist_carry_ids.split(',');
+
+        console.log(c.assist_carry_times);
+        console.log(c.assist_carry_ids);
 
         switch(c.team){
             case 0: {  bgColor = "team-red"; } break;
@@ -112,6 +123,7 @@ const MatchCTFCaps = ({players, caps, matchStart, totalTeams}) =>{
             currentName = playerNames.get(parseInt(c.assists[x]));
 
             if(currentName !== undefined){
+
                 assistElems.push(
                     <span key={`assists_team_${c.team}_${currentName.name}_${x}`}>
                         <CountryFlag country={currentName.country}/>
@@ -119,6 +131,7 @@ const MatchCTFCaps = ({players, caps, matchStart, totalTeams}) =>{
                         {(x < c.assists.length - 1) ? ',' : ''}
                     </span>
                 );
+
             }else{
                 /*if(c.assists[x] !== ''){
                     assistElems.push(
@@ -132,7 +145,21 @@ const MatchCTFCaps = ({players, caps, matchStart, totalTeams}) =>{
             }
         }
 
+        for(let x = 0; x < c.assist_carry_times.length; x++){
+
+            if(c.assist_carry_ids[x] !== ''){
+
+                totalCarryTime += parseFloat(c.assist_carry_times[x]);
+                currentName = playerNames.get(parseInt(c.assist_carry_ids[x]));
+       
+                carryElems.push(<span>
+                    <CountryFlag country={currentName.country}/>{currentName.name} <i className="yellow">{(parseFloat(c.assist_carry_times[x])).toFixed(2)}</i><br/>
+                </span>);
+            }
+        }
+
         teamScoreString = '';
+
         for(let t = 0; t < totalTeams; t++){
 
             if(t > 0){
@@ -141,6 +168,11 @@ const MatchCTFCaps = ({players, caps, matchStart, totalTeams}) =>{
                 teamScoreString += `${teamScores[t]}`;
             }
         }
+
+        if(totalCarryTime > 0){
+            totalDropTime = c.travel_time - totalCarryTime
+        }
+
         elems.push(<tr key={`tr_${c.team}_${i}`} className={bgColor}>
             <td className="text-left"><CountryFlag country={grab.country}/><a href={`/player/${c.grab}`} >{grab.name}</a></td>
             <td><MMSS timestamp={c.grab_time - matchStart}/> </td>
@@ -148,6 +180,8 @@ const MatchCTFCaps = ({players, caps, matchStart, totalTeams}) =>{
             <td>{assistElems}</td>
             <td className="text-left"><CountryFlag country={cap.country} /><a href={`/player/${c.cap}`} >{cap.name}</a></td>
             <td><MMSS timestamp={c.cap_time - matchStart}/></td>
+            <td>{carryElems}</td>
+            <td>{totalDropTime.toFixed(2)} Seconds</td>
             <td>{c.travel_time} Seconds</td>
             <td>{teamScoreString}</td>
         </tr>);
@@ -160,14 +194,16 @@ const MatchCTFCaps = ({players, caps, matchStart, totalTeams}) =>{
         <table>
             <tbody>
                 <tr>
-                    <TipHeader title="Grab" content=""/>
-                    <TipHeader title="Grab Time" content=""/>
-                    <TipHeader title="Covers" content=""/>
-                    <TipHeader title="Assists" content=""/>
-                    <TipHeader title="Capture" content=""/>
-                    <TipHeader title="Capture Time" content=""/>
-                    <TipHeader title="Travel Time" content=""/>
-                    <TipHeader title="Scores at Cap" content=""/>
+                    <TipHeader title="Grab" content="The player who took the flag from the enemy base."/>
+                    <TipHeader title="Grab Time" content="Time of the match when the flag was taken from the base."/>
+                    <TipHeader title="Covers" content="Players who killed enemies after the flag carrier."/>
+                    <TipHeader title="Assists" content="Players who carried the flag at some point."/>
+                    <TipHeader title="Capture" content="Player who captured the flag."/>
+                    <TipHeader title="Capture Time" content="Time of the match when the flag was captured"/>
+                    <TipHeader title="Carry Times" content="In seconds how long a player had the flag before capping/dropping it."/>
+                    <TipHeader title="Time Dropped" content="How long the flag was dropped." />
+                    <TipHeader title="Travel Time" content="How long it took from grab to capture in seconds."/>
+                    <TipHeader title="Scores at Cap" content="Team scores at the time of capture."/>
                 </tr>
                 {elems}
             </tbody>
