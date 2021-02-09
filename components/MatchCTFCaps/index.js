fd3c3e29a4187a9a1506b17d9eaa2f101aada790
Scott Adkin <scottadkin001@gmail.com>
2021-02-09 06:15:46 +0000

    changed flag cap display again

--- components/MatchCTFCaps/index.js
+++ components/MatchCTFCaps/index.js
@@ -3,6 +3,7 @@ import CountryFlag from '../CountryFlag/';
 import MMSS from '../MMSS/';
 import styles from './MatchCTFCaps.module.css';
 import Link from 'next/link';
+import Functions from '../../api/functions'
 
 const createPlayerMap = (players) =>{
 
@@ -33,7 +34,7 @@ const getPlayer = (players, id) =>{
     return {"name": 'Not Found', "country": 'xx'};
 }
 
-const MatchCTFCaps = ({players, caps, matchStart, totalTeams}) =>{
+/*const MatchCTFCaps = ({players, caps, matchStart, totalTeams}) =>{
 
     players = JSON.parse(players);
     caps = JSON.parse(caps);
@@ -264,5 +265,186 @@ const MatchCTFCaps = ({players, caps, matchStart, totalTeams}) =>{
         </table>
 */
 
+
+function createCovers(covers){
+
+    const data = new Map();
+
+    covers = covers.split(',');
+
+    let player = 0;
+    let current = 0;
+
+    for(let i = 0; i < covers.length; i++){
+
+        if(covers[i] === ''){
+            continue;
+        }
+
+        player = parseInt(covers[i]);
+        current = data.get(player);
+
+        if(current === undefined){
+            data.set(player, 1);
+        }else{
+            data.set(player, ++current)
+        }
+    }
+
+    let ordered = [];
+
+    for(const [key, value] of data){
+        ordered.push({"key": key, "value": value});
+    }
+
+    ordered.sort((a, b) =>{
+
+        a = a.value;
+        b = b.value;
+
+        if(a > b){
+            return -1;
+        }else if(a < b){
+            return 1;
+        }
+
+        return 0;
+    });
+
+    data.clear();
+
+    for(let i = 0; i < ordered.length; i++){
+
+        data.set(ordered[i].key, ordered[i].value);
+    }
+
+    return data;
+}
+
+function createAssists(assists){
+
+    assists = assists.split(',');
+
+    const players = [];
+
+    for(let i = 0; i < assists.length; i++){
+
+        if(assists[i] === ''){
+            continue;
+        }
+
+        if(players.indexOf(parseInt(assists[i])) === -1){
+            players.push(parseInt(assists[i]));
+        }
+
+    }
+
+    return players;
+}
+
+
+function calcDropTime(data){
+
+    let totalCarryTime = 0;
+
+    data.assist_carry_times = data.assist_carry_times.split(',');
+
+    for(let i = 0; i < data.assist_carry_times.length; i++){
+
+        if(data.assist_carry_times[i] !== ''){
+            totalCarryTime += parseFloat(data.assist_carry_times[i]);
+        }
+    }
+   
+
+    return parseFloat(data.travel_time - totalCarryTime).toFixed(2);
+}
+
+const MatchCTFCaps = ({players, caps, matchStart, totalTeams}) =>{
+
+    players = JSON.parse(players);
+    caps = JSON.parse(caps);
+    matchStart = parseFloat(matchStart)
+
+    const playerNames = createPlayerMap(players);
+
+    let currentCovers = 0;
+    let currentAssists = 0;
+    let grabPlayer = 0;
+    let capPlayer = 0;
+    let bgColor = 0;
+    let currentCoverPlayer = 0;
+    let currentAssistPlayer = 0;
+    let totalDropTime = 0;
+
+  
+    const elems = [];
+    let coverElems = [];
+    let assistElems = [];
+
+    let c = 0;
+
+    for(let i = 0; i < caps.length; i++){
+
+        c = caps[i];
+
+        currentCovers = createCovers(c.covers);
+        currentAssists = createAssists(c.assists);
+        totalDropTime = calcDropTime(c);
+
+        grabPlayer = getPlayer(playerNames, c.grab);
+        capPlayer = getPlayer(playerNames, c.cap);
+
+        coverElems = [];
+        assistElems = [];
+        
+
+        for(const [key, value] of currentCovers){
+
+            currentCoverPlayer = getPlayer(playerNames, key);
+
+            coverElems.push(<span key={`cap-${i}-cover-${key}`} className={styles.cover}>
+                <CountryFlag country={currentCoverPlayer.country}/><Link href={`/player/${key}`}><a>{currentCoverPlayer.name}</a></Link> <i>({value})</i>
+            </span>);
+        }
+
+        for(let x = 0; x < currentAssists.length; x++){
+
+            currentAssistPlayer = getPlayer(playerNames, currentAssists[x]);
+
+            assistElems.push(<span key={`cap-${i}-assist-${x}`} className={styles.cover}>
+                <CountryFlag country={currentAssistPlayer.country}/><Link href={`/player/${currentAssists[x]}`}><a>{currentAssistPlayer.name}</a></Link>
+            </span>);
+        }
+
+        bgColor = Functions.getTeamColor(c.team);
+
+        elems.push(<tr key={`cover-${i}`} className={bgColor}>
+            <td><span className={styles.time}><MMSS timestamp={c.grab_time - matchStart}/></span><CountryFlag country={grabPlayer.country}/><Link href={`/player/${c.grab}`}><a>{grabPlayer.name}</a></Link></td>
+            <td>{coverElems}</td>
+            <td>{assistElems}</td>
+            <td><span className={styles.time}><MMSS timestamp={c.cap_time - matchStart}/></span><CountryFlag country={capPlayer.country}/><Link href={`/player/${c.cap}`}><a>{capPlayer.name}</a></Link></td>
+            <td><span class={styles.time}><MMSS timestamp={c.travel_time}/></span></td>
+            <td className={styles.time}>{(totalDropTime > 0) ? `${totalDropTime} Seconds` : ''}</td>
+        </tr>);
+    }
+
+    return (<div className={`special-table ${styles.table}`}>
+        <div className="default-header">Flag Caps</div> 
+        <table>
+            <tbody>
+                <tr>
+                    <th>Grabbed</th>
+                    <th>Covers</th>
+                    <th>Assists</th>
+                    <th>Capped</th>
+                    <th>Travel Time</th>
+                    <th>Time Dropped</th>
+                </tr>
+                {elems}
+            </tbody>
+        </table>       
+    </div>);
+}
 
 export default MatchCTFCaps;
