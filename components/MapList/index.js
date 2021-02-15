2569ad37ab1528fb46eeda4543dc0efa43ed1f94
Scott Adkin <scottadkin001@gmail.com>
2021-02-15 15:17:37 +0000

    maps table view

--- components/MapList/index.js
+++ components/MapList/index.js
@@ -1,4 +1,6 @@
 import MapDefaultBox from '../MapDefaultBox/';
+import MapTableRow from '../MapTableRow/';
+import styles from './MapList.module.css';
 
 class MapList extends React.Component{
 
@@ -14,12 +16,32 @@ class MapList extends React.Component{
         const maps = JSON.parse(this.props.data);
 
         for(let i = 0; i < maps.length; i++){
+            elems.push(<MapTableRow data={maps[i]}/>);
+        }
+
+        return (<div className="special-table">
+            <table className={styles.table}>
+                <tbody>
+                    <tr>
+                        <th>Name</th>
+                        <th>First</th>
+                        <th>Last</th>
+                        <th>Playtime</th>
+                        <th>Matches</th>
+                    </tr>
+                    {elems}
+                </tbody>
+            </table>
+        </div>);
+
+        /*
+        for(let i = 0; i < maps.length; i++){
             elems.push(<MapDefaultBox key={i} data={maps[i]} images={this.props.images}/>);
         }
 
         return (<div>
             {elems}
-        </div>);
+        </div>);*/
     }
 }
 
---
+++ components/MapTableRow/index.js
@@ -0,0 +1,22 @@
+import TimeStamp from '../timestamp';
+import Functions from '../../api/functions';
+
+class MapTableRow extends React.Component{
+
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+
+        return (<tr>
+            <td>{Functions.removeUnr(this.props.data.name)}</td>
+            <td><TimeStamp timestamp={this.props.data.first} /></td>
+            <td><TimeStamp timestamp={this.props.data.last} /></td>
+            <td>{parseFloat(this.props.data.playtime / (60 * 60)).toFixed(2)} Hours</td>
+            <td>{this.props.data.matches}</td>
+        </tr>);
+    }
+}
+
+export default MapTableRow;
