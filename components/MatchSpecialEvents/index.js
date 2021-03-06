d2ff29a425bbdcbb620753ad7d9880ba00c80488
Scott Adkin <scottadkin001@gmail.com>
2021-03-06 03:27:42 +0000

    start of dynamic special events

--- components/MatchSpecialEvents/index.js
+++ components/MatchSpecialEvents/index.js
@@ -1,8 +1,9 @@
 import TipHeader from '../TipHeader/';
 import CountryFlag from '../CountryFlag/';
 import styles from './MatchSpecialEvents.module.css';
+import React from 'react';
 
-
+/*
 const bAllEmpty = (data, bSpree) =>{
 
     if(bSpree){
@@ -156,6 +157,201 @@ const MatchSpecialEvents = ({bTeamGame, players}) =>{
         </div>
     );
 }
+*/
 
+class MatchSpecialEvents extends React.Component{
+
+    constructor(props){
+
+        super(props);
+
+        this.players = JSON.parse(props.players);
+        this.bTeamGame = props.bTeamGame;
+
+        this.state = {"mode": 2};
+
+        console.log(this.players);
+
+        this.changeMode = this.changeMode.bind(this);
+
+    }
+
+
+    changeMode(value){
+
+        this.setState({"mode": value});
+    }
+
+    bAnyMultis(player){
+
+
+        for(let i = 1; i < 8; i++){
+
+            if(player[`multi_${i}`] > 0){
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    createMultiElems(){
+
+        let elems = [];
+
+        let p = 0;
+
+        for(let i = 0; i < this.players.length; i++){
+
+            p = this.players[i];
+
+            if(this.bAnyMultis(p)){
+
+                if(this.state.mode === 2){
+
+                    elems.push(<tr>
+                        <td>{p.name}</td>
+                        <td>{p.multi_1}</td>
+                        <td>{p.multi_2}</td>
+                        <td>{p.multi_3}</td>
+                        <td>{p.multi_4}</td>
+                        <td>{p.multi_5}</td>
+                        <td>{p.multi_6}</td>
+                        <td>{p.multi_7}</td>
+                        <td>{p.multi_best}</td>
+                    </tr>);
+
+                }else if(this.state.mode === 0){
+
+                    elems.push(<tr>
+                        <td>{p.name}</td>
+                        <td>{p.multi_1}</td>
+                        <td>{p.multi_2}</td>
+                        <td>{p.multi_3}</td>
+                        <td>{p.multi_4 + p.multi_5 + p.multi_6 + p.multi_7}</td>
+                        <td>{p.multi_best}</td>
+                    </tr>);
+
+                }else if(this.state.mode === 3){
+
+                    elems.push(<tr>
+                        <td>{p.name}</td>
+                        <td>{p.multi_1}</td>
+                        <td>{p.multi_2}</td>
+                        <td>{p.multi_3}</td>
+                        <td>{p.multi_4 }</td>
+                        <td>{p.multi_5 + p.multi_6 + p.multi_7}</td>
+                        <td>{p.multi_best}</td>
+                    </tr>);
+
+                }else if(this.state.mode === 1){
+
+                    elems.push(<tr>
+                        <td>{p.name}</td>
+                        <td>{p.multi_1}</td>
+                        <td>{p.multi_2}</td>
+                        <td>{p.multi_3}</td>
+                        <td>{p.multi_4 }</td>
+                        <td>{p.multi_5 }</td>
+                        <td>{p.multi_6 + p.multi_7}</td>
+                        <td>{p.multi_best}</td>
+                    </tr>);
+                }
+            }
+        
+        }
+
+        if(elems.length > 0){
+
+            if(this.state.mode === 2){
+
+                elems.unshift(<tr>
+                    <th>Player</th>
+                    <TipHeader title={"Double Kill"} content="Player Killed 2 Players in a short amount of time without dying."/>
+                    <TipHeader title={"Multi Kill"} content="Player Killed 3 Players in a short amount of time without dying."/>
+                    <TipHeader title={"Mega Kill"} content="Player Killed 4 Players in a short amount of time without dying."/>
+                    <TipHeader title={"Ultra Kill"} content="Player Killed 5 Players in a short amount of time without dying."/>
+                    <TipHeader title={"Monster Kill"} content="Player Killed 6 Players in a short amount of time without dying."/>
+                    <TipHeader title={"Ludicirous Kill"} content="Player Killed 7 Players in a short amount of time without dying."/>
+                    <TipHeader title={"Holy Shit"} content="Player Killed 8 or more Players in a short amount of time without dying."/>
+                    <TipHeader title={"Best"} content="Most players killed in a short amount of time without dying."/>
+                </tr>);
+
+            }else if(this.state.mode === 0){
+
+                elems.unshift(<tr>
+                    <th>Player</th>
+                    <TipHeader title={"Double Kill"} content="Player Killed 2 Players in a short amount of time without dying."/>
+                    <TipHeader title={"Multi Kill"} content="Player Killed 3 Players in a short amount of time without dying."/>
+                    <TipHeader title={"Ultra Kill"} content="Player Killed 4 Players in a short amount of time without dying."/>
+                    <TipHeader title={"Monster Kill"} content="Player Killed 5 or more Players in a short amount of time without dying."/>
+                    <TipHeader title={"Best"} content="Most players killed in a short amount of time without dying."/>
+                </tr>);
+
+            }else if(this.state.mode === 3){
+
+                elems.unshift(<tr>
+                    <th>Player</th>
+                    <TipHeader title={"Double Kill"} content="Player Killed 2 Players in a short amount of time without dying."/>
+                    <TipHeader title={"Multi Kill"} content="Player Killed 3 Players in a short amount of time without dying."/>
+                    <TipHeader title={"Mega Kill"} content="Player Killed 4 Players in a short amount of time without dying."/>
+                    <TipHeader title={"Ultra Kill"} content="Player Killed 5 Players in a short amount of time without dying."/>
+                    <TipHeader title={"Monster Kill"} content="Player Killed 6 or more Players in a short amount of time without dying."/>
+                    <TipHeader title={"Best"} content="Most players killed in a short amount of time without dying."/>
+                </tr>);
+
+            }else if(this.state.mode === 1){
+
+                elems.unshift(<tr>
+                    <th>Player</th>
+                    <TipHeader title={"Double Kill"} content="Player Killed 2 Players in a short amount of time without dying."/>
+                    <TipHeader title={"Triple Kill"} content="Player Killed 3 Players in a short amount of time without dying."/>
+                    <TipHeader title={"Multi Kill"} content="Player Killed 4 Players in a short amount of time without dying."/>
+                    <TipHeader title={"Mega Kill"} content="Player Killed 5 Players in a short amount of time without dying."/>
+                    <TipHeader title={"Ultra Kill"} content="Player Killed 6 or more Players in a short amount of time without dying."/>
+                    <TipHeader title={"Monster Kill"} content="Player Killed 7 or more Players in a short amount of time without dying."/>
+                    <TipHeader title={"Best"} content="Most players killed in a short amount of time without dying."/>
+                </tr>);
+            }
+        }
+
+        if(elems.length > 0){
+            return <table>
+                <tbody>
+                    {elems}
+                </tbody>
+            </table>
+
+        }else{
+            return [];
+        }
+    }
+
+    render(){
+
+
+        const multiElems = this.createMultiElems();
+        const spreeElems = [];
+
+        return (<div>
+            <div className="default-header">Special Events</div>
+
+            <div className={styles.button} onClick={(() =>{
+                this.changeMode(0)
+            })}>Classic</div>
+            <div className={styles.button} onClick={(() =>{
+                this.changeMode(1)
+            })}>SmartCTF/DM</div>
+             <div className={styles.button} onClick={(() =>{
+                this.changeMode(2)
+            })}>UT2K4</div>
+             <div className={styles.button} onClick={(() =>{
+                this.changeMode(3)
+            })}>UT3</div>
+
+            {multiElems}
+        </div>);
+    }
+}
 
 export default MatchSpecialEvents;
