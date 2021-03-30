18c2705f4d4dd28a973cc17207390c5ebbff66cf
Scott Adkin <scottadkin001@gmail.com>
2021-03-30 13:26:19 +0100

    lazy fix

--- components/MapAssaultObjectives/index.js
+++ components/MapAssaultObjectives/index.js
@@ -31,15 +31,34 @@ const MapAssaultObjectives = ({objects, mapPrefix, mapName, images}) =>{
 
     let percent = 0;
 
+    console.log(mapName);
+
     for(let i = 0; i < objects.length; i++){
 
         o = objects[i];
+
+        if(mapName === "rook"){
+
+            if(i === 1){
+
+                elems.push(<div key={"rook-1"} className={styles.box}>
+                    <div>
+                        <Image src={getObjectImage(images, mapName, 1)} width="200" height="112" />
+                    </div>
+                    <div>
+                        <span className={styles.name}>Gatehouse Chains</span><br/>
+                        N/A
+                    </div>
+                </div>);
+
+            }
+        }
         
         percent = ((o.taken / o.matches) * 100).toFixed(2);
 
         if(o.taken === 0) percent = 0;
 
-        elems.push(<div className={styles.box}>
+        elems.push(<div key={i} className={styles.box}>
             <div>
                 <Image src={getObjectImage(images, mapName, o.obj_id)} width="200" height="112" />
             </div>
@@ -49,6 +68,38 @@ const MapAssaultObjectives = ({objects, mapPrefix, mapName, images}) =>{
                 Taken in {percent}% of Matches
             </div>
         </div>);
+
+        
+        if(i == objects.length - 1){
+
+            if(mapName === "rook"){
+
+                elems.push(<div key={"rook-end"} className={styles.box}>
+                    <div>
+                        <Image src={getObjectImage(images, mapName, 5)} width="200" height="112" />
+                    </div>
+                    <div>
+                        <span className={styles.name}>End</span><br/>
+                        N/A
+                    </div>
+                </div>);
+
+            }else if(mapName === "overlord"){
+                elems.push(<div key={"rook-end"} className={styles.box}>
+                    <div>
+                        <Image src={getObjectImage(images, mapName, 3)} width="200" height="112" />
+                    </div>
+                    <div>
+                        <span className={styles.name}>End</span><br/>
+                        N/A
+                    </div>
+                </div>);
+            }
+            
+        }
+        
+
+        
     }
 
     return <div className="m-bottom-10">
