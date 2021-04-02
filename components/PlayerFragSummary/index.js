8767860dd5fa3b7d12c146cbe3c84ba7c4827a9c
Scott Adkin <scottadkin001@gmail.com>
2021-04-02 14:54:14 +0100

    added tabs to frag summary

--- components/PlayerFragSummary/index.js
+++ components/PlayerFragSummary/index.js
@@ -1,66 +1,89 @@
 import styles from '../PlayerSummary/PlayerSummary.module.css';
+import React from 'react';
 
-const PlayerFragSummary = ({
-    score, frags, kills, deaths, suicides, teamKills, spawnKills, efficiency,
-    firstBlood, accuracy, close, long, uber, headshots, spawnKillSpree
-}) =>{
+class PlayerFragSummary extends React.Component{
+
+    constructor(props){
+
+        super(props);
+
+        this.state = {"mode": 0};
+
+        this.changeMode = this.changeMode.bind(this);
+    }
+
+    changeMode(id){
+        this.setState({"mode": id});
+    }
 
 
-    return (
-        <div className={`${styles.table} special-table`}>
-            <div className="default-header">
-                Frag Performance
-            </div>       
-            <table className="t-width-1 m-bottom-10">
+    displayGeneral(){
 
-                <tbody>
-                    <tr>
-                        <th>Score</th>
-                        <th>Frags</th>
-                        <th>Suicides</th>
-                        <th>Team Kills</th>
-                        <th>Kills</th>
-                        <th>Deaths</th>  
-                        <th>Efficiency</th>
-                        <th>Last Accuracy</th>
-                    </tr>
-                    <tr>
-                        <td>{score}</td>
-                        <td>{frags}</td>
-                        <td>{suicides}</td>
-                        <td>{teamKills}</td>
-                        <td>{kills}</td>
-                        <td>{deaths}</td>  
-                        <td>{efficiency.toFixed(2)}%</td>
-                        <td>{accuracy.toFixed(2)}%</td>
-                    </tr>
-                </tbody>
-            </table>      
-            <div className="default-header">
-                Extended Frag Performance
+        return <table className="t-width-1">
+            <tbody>
+                <tr>
+                    <th>Score</th>
+                    <th>Frags</th>
+                    <th>Suicides</th>
+                    <th>Team Kills</th>
+                    <th>Kills</th>
+                    <th>Deaths</th>  
+                    <th>Efficiency</th>
+                    <th>Last Accuracy</th>
+                </tr>
+                <tr>
+                    <td>{this.props.score}</td>
+                    <td>{this.props.frags}</td>
+                    <td>{this.props.suicides}</td>
+                    <td>{this.props.teamKills}</td>
+                    <td>{this.props.kills}</td>
+                    <td>{this.props.deaths}</td>  
+                    <td>{this.props.efficiency.toFixed(2)}%</td>
+                    <td>{this.props.accuracy.toFixed(2)}%</td>
+                </tr>
+            </tbody>
+        </table>      
+    }
+
+    displayExtended(){
+
+        return <table className="t-width-1">
+            <tbody>
+                <tr>
+                    <th>Headshots</th>
+                    <th>Spawn Kills</th>
+                    <th>Best Spawn Kill Spree</th>
+                    <th>Close Range Kills</th>
+                    <th>Long Range Kills</th>
+                    <th>Uber Long Range kills</th>
+                </tr>
+                <tr>
+                    <td>{this.props.headshots}</td>
+                    <td>{this.props.spawnKills}</td>
+                    <td>{this.props.spawnKillSpree} Kills</td>
+                    <td>{this.props.close}</td>
+                    <td>{this.props.long}</td>
+                    <td>{this.props.uber}</td>
+                </tr>
+            </tbody>
+        </table>
+    }
+
+    render(){
+        return <div className="special-table">
+            <div className="default-header">Frag Summary</div>
+            <div className="tabs">
+                <div className={`tab ${(this.state.mode === 0) ? "tab-selected" : ""}`} onClick={(() =>{
+                    this.changeMode(0);
+                })}>General</div>
+                <div className={`tab ${(this.state.mode === 1) ? "tab-selected" : ""}`}  onClick={(() =>{
+                    this.changeMode(1);
+                })}>Extended</div>
             </div>
-            <table className="t-width-1">
-                <tbody>
-                    <tr>
-                        <th>Headshots</th>
-                        <th>Spawn Kills</th>
-                        <th>Best Spawn Kill Spree</th>
-                        <th>Close Range Kills</th>
-                        <th>Long Range Kills</th>
-                        <th>Uber Long Range kills</th>
-                    </tr>
-                    <tr>
-                        <td>{headshots}</td>
-                        <td>{spawnKills}</td>
-                        <td>{spawnKillSpree} Kills</td>
-                        <td>{close}</td>
-                        <td>{long}</td>
-                        <td>{uber}</td>
-                    </tr>
-                </tbody>
-            </table>
+
+            {(this.state.mode === 0) ? this.displayGeneral() : this.displayExtended()}
         </div>
-    );
+    }
 }
 
 export default PlayerFragSummary;
