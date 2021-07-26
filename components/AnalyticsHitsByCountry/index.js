6cb8013daf38a2b818d8a81b5875f5ac6c73aa6c
Scott Adkin <scottadkin001@gmail.com>
2021-07-26 19:15:27 +0100

    Added totals summary to country hits

--- components/AnalyticsHitsByCountry/index.js
+++ components/AnalyticsHitsByCountry/index.js
@@ -7,9 +7,26 @@ const AnalyticsHitsByCountry = ({data}) =>{
 
     let d = 0;
 
+    let total = 0;
+
+    let first = null;
+    let last = 0;
+
     for(let i = 0; i < data.length; i++){
 
         d = data[i];
+
+        if(first === null){
+            first = d.first;
+        }else{
+            if(d.first < first) first = d.first;
+        }
+
+        if(d.last > last){
+            last = d.last;
+        }
+
+        total += d.total;
 
         rows.push(<tr key={i}>
             <td><CountryFlag country={d.code}/>{d.country}</td>
@@ -18,6 +35,13 @@ const AnalyticsHitsByCountry = ({data}) =>{
             <td>{d.total}</td>
         </tr>);
     }
+
+    rows.push(<tr key={"totals"}>
+        <td className="team-green">Total</td>
+        <td className="team-green">{Functions.convertTimestamp(first)}</td>
+        <td className="team-green">{Functions.convertTimestamp(last)}</td>
+        <td className="team-green">{total}</td>
+    </tr>);
 
     return <div>
         <div className="default-header">Hits By Country</div>
