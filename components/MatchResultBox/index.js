4b40a6f3f30d96ba8c89840b782d1c656882c271
Scott Adkin <scottadkin001@gmail.com>
2021-09-22 04:25:05 +0100

    Changed matchresultbox style

--- components/MatchResultBox/index.js
+++ components/MatchResultBox/index.js
@@ -1,6 +1,7 @@
 import styles from './MatchResultBox.module.css';
 import Image from 'next/image';
 import MatchResult from '../MatchResult';
+import Functions from '../../api/functions';
 
 const MatchResultBox = ({serverName, gametypeName, mapName, mapImage, date, players, playtime, totalTeams, result, dmScore, monsterHunt, endReason}) =>{
 
@@ -22,13 +23,25 @@ const MatchResultBox = ({serverName, gametypeName, mapName, mapImage, date, play
     let shortenedName = serverName.slice(0, 65);
 
     if(serverName !== shortenedName){
-
         serverName = `${shortenedName}...`;
     }
 
 
-    //dmWinner, dmScore, totalTeams, redScore, blueScore, greenScore, yellowScore, bMonsterHunt, endReason
     return <div className={styles.wrapper}>
+        <div className={styles.title}>{mapName}</div>
+        <div className={styles.gametype}>{gametypeName}</div>
+        <div className={styles.players}>{players} {(players !== 1) ? "Players" : "Player"}</div>
+        <div className={styles.image}>
+            <Image src={`/images/maps/${mapImage}.jpg`} width={400} height={225}/>
+        </div>
+        <div className={`${styles.server} yellow`}>{serverName}</div>
+        <div className={styles.date}>{date}</div>
+        <div className={styles.playtime}>Playtime {playtime}</div>
+        <MatchResult bMonsterHunt={monsterHunt} endReason={endReason} dmWinner={dmWinner} dmScore={(dmScore === undefined) ? null : dmScore} totalTeams={totalTeams} 
+            redScore={scores[0]} blueScore={scores[1]} greenScore={scores[2]} yellowScore={scores[3]}/>
+    </div>
+    //dmWinner, dmScore, totalTeams, redScore, blueScore, greenScore, yellowScore, bMonsterHunt, endReason
+    /*return <div className={styles.wrapper}>
         <div className={styles.content}>
             <div className={styles.gametype}><span className="yellow">{gametypeName}</span> on <span className="yellow">{mapName}</span></div>
             <Image src={`/images/maps/${mapImage}.jpg`} width={384} height={216} />
@@ -42,7 +55,7 @@ const MatchResultBox = ({serverName, gametypeName, mapName, mapImage, date, play
             redScore={scores[0]} blueScore={scores[1]} greenScore={scores[2]} yellowScore={scores[3]}/>
         </div>
         
-    </div>
+    </div>*/
 }
 
 
