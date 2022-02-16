9c288f0b24c68e3f6de0b7c7cb4a05a2a5495955
Scott Adkin <scottadkin001@gmail.com>
2022-02-16 11:34:31 +0000

    Style tweak to match summary

--- components/MatchSummary/index.js
+++ components/MatchSummary/index.js
@@ -1,7 +1,7 @@
 import styles from './MatchSummary.module.css';
-import TimeStamp from '../TimeStamp/';
 import Playtime from '../Playtime/';
 import MatchResult from '../MatchResult/';
+import Functions from '../../api/functions';
 
 
 const MatchSummary = ({info, server, gametype, map, bMonsterHunt, settings}) =>{
@@ -21,8 +21,8 @@ const MatchSummary = ({info, server, gametype, map, bMonsterHunt, settings}) =>{
 
     if(settings["Display Mutators"] === "true"){
         if(info.mutators.length > 0){
-            mutatorsElem = <div>
-                <span className="yellow">Mutators: </span>
+            mutatorsElem = <div className={styles.mutators}>
+                <span className="white">Mutators: </span>
                 {info.mutators.replace(/,/ig, ', ')}
             </div>
         }
@@ -32,29 +32,31 @@ const MatchSummary = ({info, server, gametype, map, bMonsterHunt, settings}) =>{
     let targetScoreElem = null;
 
     if(settings["Display Target Score"] === "true"){
-        targetScoreElem = (info.target_score !== 0) ? <div><span className="yellow">Target Score</span> {info.target_score}</div> : null;
+        targetScoreElem = (info.target_score !== 0) ? <div><span className="white">Target Score</span> {info.target_score}</div> : null;
     }
 
     let timeLimitElem = null;
 
     if(settings["Display Time Limit"] === "true"){
-        timeLimitElem = (info.time_limit !== 0) ? <div><span className="yellow">Time Limit</span> {info.time_limit} Minutes</div> : null;
+        timeLimitElem = (info.time_limit !== 0) ? <div><span className="white">Time Limit</span> {info.time_limit} Minutes</div> : null;
     }
 
     return (
         <div className={`${styles.wrapper} center`}>
             <div className={styles.map}>
+
                 <MatchResult teamGame={info.team_game} dmWinner={info.dm_winner} dmScore={info.dm_score} totalTeams={info.total_teams}
                 redScore={Math.floor(info.team_score_0)} blueScore={Math.floor(info.team_score_1)} greenScore={Math.floor(info.team_score_2)} 
                 yellowScore={Math.floor(info.team_score_3)} bMonsterHunt={bMonsterHunt} endReason={info.end_type}/>
+                <span className="white">{gametype}</span> {(info.insta) ? '(Instagib)' : ''} on <span className="white">{map}</span><br/>
                 <span className={styles.small}>{server}</span><br/>
-                {map}<br/>{gametype} {(info.insta) ? '(Instagib)' : ''}<br/>
-                <span className={styles.small}><TimeStamp key={info.date} timestamp={info.date}/></span><br/>
+            
+                <span className={styles.small}>{Functions.convertTimestamp(info.date, false)}</span><br/>
                 {targetScoreElem}
                 {timeLimitElem}
 
-                <span className="yellow">Match Length</span> <Playtime seconds={info.playtime} /><br/>
-                <span className="yellow">Players</span> {info.players}<br/>
+                <span className="white">Match Length</span> <Playtime seconds={info.playtime} /><br/>
+                <span className="white">Players</span> {info.players}<br/>
 
                 {mutatorsElem}
 
