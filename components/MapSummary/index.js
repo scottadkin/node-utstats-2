330c6c327dda1c67a9264ae2d1cb96e4608aaed5
Scott Adkin <scottadkin001@gmail.com>
2022-10-15 10:11:01 +0100

    Cleaned up component design

--- components/MapSummary/index.js
+++ components/MapSummary/index.js
@@ -2,63 +2,62 @@
 import styles from './MapSummary.module.css';
 import Table2 from '../Table2';
 import Functions from '../../api/functions';
-import Link from 'next/link';
 import Image from 'next/image';
+import TableHeader from '../TableHeader';
+
 
 const MapSummary = ({basic, spawns, imageHost, image}) =>{
 
-    return <div className={`${styles.top} m-bottom-10`}>
-        <Image onClick={(() =>{
+    const totalSpawns = JSON.parse(spawns).length;
+
+    return <div className={`${styles.wrapper}`}>
+        <TableHeader width={2}>{Functions.removeUnr(basic.name)} Summary</TableHeader>
+        <div className={`${styles.image} t-width-2 center`}>
+            <Image id="main-image" src={`/${image}`} alt="Map image" width={1920} height={1080} onClick={(() =>{
             const elem = document.getElementById("main-image");
             elem.requestFullscreen();
-        })} className={styles.mimage} id="main-image" src={`/${image}`} alt="image" width={1920} height={1080}/>
-        <Table2 noBottomMargin={true}>
-            <tr>
-                <td>Name</td>
-                <td>{Functions.removeUnr(basic.name)}</td>
-            </tr>
+        })}/>
+        </div>
+        <Table2 width={2}>
             <tr>
                 <td>Title</td>
-                <td>{basic.title}</td>
+                <td>{basic.title}</td>   
             </tr>
             <tr>
                 <td>Author</td>
-                <td>{basic.author}</td>
-            </tr>
-            <tr>
-                <td>Ideal Player Count</td>
-                <td>{basic.ideal_player_count}</td>
+                <td>{basic.author}</td>   
             </tr>
             <tr>
                 <td>Level Enter Text</td>
-                <td>{basic.level_enter_text}</td>
+                <td>{basic.level_enter_text}</td>   
             </tr>
             <tr>
-                <td>Total Matches</td>
-                <td>{basic.matches}</td>
-            </tr>
-            <tr>
-                <td>Total Playtime</td>
-                <td>{parseFloat(basic.playtime / (60 * 60)).toFixed(2)} Hours</td>
-            </tr>
-            <tr>
-                <td>Longest Match</td>
-                <td><Link href={`/match/${basic.longestId}`}><a>{Functions.MMSS(basic.longest)}</a></Link></td>
+                <td>Ideal Player Count</td>
+                <td>{basic.ideal_player_count}</td>   
             </tr>
             <tr>
                 <td>First Match</td>
-                <td>{Functions.convertTimestamp(basic.first, true)}</td>
+                <td>{Functions.convertTimestamp(basic.first, true)}</td>   
             </tr>
             <tr>
-                <td>Last Match</td>
-                <td>{Functions.convertTimestamp(basic.last, true)}</td>
+                <td>Lastest Match</td>
+                <td>{Functions.convertTimestamp(basic.last, true)}</td>   
             </tr>
             <tr>
-                <td>Spawn Points</td>
-                <td>{JSON.parse(spawns).length}</td>
+                <td>Total Matches</td>
+                <td>{basic.matches}</td>   
+            </tr>
+            <tr>
+                <td>Total Playtime</td>
+                <td>{Functions.toHours(basic.playtime)} Hours</td>   
+            </tr>
+            <tr>
+                <td>Total Spawns</td>
+                <td>{totalSpawns}</td>   
             </tr>
         </Table2>
     </div>
 }
+
 
 export default MapSummary;
