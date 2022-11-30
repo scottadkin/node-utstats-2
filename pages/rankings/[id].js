3973460681c0b00ee4f7144a0e76ce6a91db5706
Scott Adkin <scottadkin001@gmail.com>
2022-11-30 20:27:42 +0000

    Changed behaviour of main page with 1 gametype

--- pages/rankings/[id].js
+++ pages/rankings/[id].js
@@ -62,20 +62,21 @@ class Rankings extends React.Component{
 
     createElems(){
 
+
         const data = JSON.parse(this.props.data);
         const gametypeNames = JSON.parse(this.props.gametypeNames);
 
-        const elems = [];
+        const bDisplayPagination = data.length === 1;
 
-        let d = 0;
+        const elems = [];
 
         for(let i = 0; i < data.length; i++){
 
-            d = data[i];
+            const d = data[i];
 
             elems.push(<RankingTable host={Functions.getImageHostAndPort(this.props.host)} gametypeId={d.id} page={this.props.page-1} perPage={this.props.perPage} 
                 key={i} mode={this.props.gametypeId}
-                title={this.getGametypeName(gametypeNames, d.id)} data={d.data} results={d.results} bDisplayPagination={(data.length > 1) ? false : true}
+                title={this.getGametypeName(gametypeNames, d.id)} data={d.data} results={d.results} bDisplayPagination={bDisplayPagination}
             />);
         }
 
@@ -199,7 +200,14 @@ export async function getServerSideProps({req, query}){
     let data = [];
 
     if(gametype === 0){
+
+        if(gametypeIds.length <= 1){
+            perPage = parseInt(pageSettings["Rankings Per Page (Individual)"]);
+        }
+
         data = await rankingManager.getMultipleGametypesData(gametypeIds, perPage);
+        
+    
     }else{
         data.push({"id": gametype, "data": await rankingManager.getData(gametype, page, perPage)});
     }
