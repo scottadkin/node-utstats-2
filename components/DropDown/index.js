236fbf4a55b99df1d32a0fdf2b44cfc44db266dc
Scott Adkin <scottadkin001@gmail.com>
2022-12-09 17:08:17 +0000

    Somehwhat working dropdown

--- components/DropDown/index.js
+++ components/DropDown/index.js
@@ -10,6 +10,7 @@ class DropDown extends React.Component{
 
         this.changeActive = this.changeActive.bind(this);
         this.changeSelected = this.changeSelected.bind(this);
+        this.hide = this.hide.bind(this);
     }
 
     changeActive(){
@@ -18,28 +19,59 @@ class DropDown extends React.Component{
         this.setState({"bActive": !previous});
     }
 
+    hide(){
+
+        this.setState({"bActive": false});
+    }
+
     changeSelected(value){
+
         if(!this.state.bActive) return;
         this.setState({"selectedValue": value});
+
+        this.props.changeSelected(this.props.fName, value);
     }
+
 
     renderEntries(){
 
         const elems = [];
 
-        let index = 0;
 
-        for(const [key, value] of Object.entries(this.props.data)){
+        for(const [value, displayValue] of Object.entries(this.props.data)){
 
-            if(!this.state.bActive && index > 0) break;
+            if(!this.state.bActive){
 
-            elems.push(<div className={styles.entry} key={key} onClick={(() =>{
-                this.changeSelected(key);
-            })}>{value}</div>);
-            index++;
+                if(this.state.selectedValue === null){
+
+                    elems.push(<div className={styles.fake} key={value}>{displayValue}</div>);
+                    break;
+
+                }else{           
+
+                    if(value === this.state.selectedValue){
+                        elems.push(<div className={styles.fake} key={value}>{displayValue}</div>);
+                        break;
+                    }
+                }
+
+            }else{
+
+                let className = `${styles.entry}`;
+
+                if(value === this.state.selectedValue){
+                    className += ` ${styles.selected}`
+                }
+
+                elems.push(<div className={className} key={value} onClick={(() =>{
+                    this.changeSelected(value);
+                })}>{displayValue}</div>);
+            }
         }
 
-        return <div className={styles.entries} onClick={this.changeActive}>
+        const zStyle = (this.state.bActive) ? {"position":"relative", "width": "100%"} : {};
+
+        return <div className={styles.entries} style={zStyle} onClick={this.changeActive}>
             {elems}
         </div>
     }
@@ -48,13 +80,13 @@ class DropDown extends React.Component{
 
         
 
-        return <div>{this.state.selectedValue}
-            <div className={styles.wrapper}>
+        return <div className={styles.wrapper}>  
             <div className={styles.label}>
                 {this.props.dName}
             </div>
-            {this.renderEntries()}
-        </div>
+            <div className={styles.dd}>
+                {this.renderEntries()}
+            </div>
         </div>
     }
 }
