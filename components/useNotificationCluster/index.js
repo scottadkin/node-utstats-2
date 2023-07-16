7d7638ae0cd3614617169090a2f8c852c8ef27a9
Scott Adkin <scottadkin001@gmail.com>
2023-07-16 03:31:55 +0100

    cleanup

--- components/useNotificationCluster/index.js
+++ components/useNotificationCluster/index.js
@@ -1,32 +1,31 @@
-const addNotification = (notifications, type, content) =>{
-
-    const current = {
-        "type": type, 
-        "content": content,
-        "bDisplay": true,
-        "id": notifications.length
-    };
-
-    return [...notifications, current];
-}
-
-const hideNotification = (notifications, targetId) =>{
-
-    const result = [];
-
-    for(let i = 0; i < notifications.length; i++){
-
-        const n = notifications[i];
-
-        if(n.id === targetId) n.bDisplay = false;
-        result.push(n);
-    }
-
-    return result;
-}
-
 const useNotificationCluster = () =>{
 
+    const addNotification = (notifications, type, content) =>{
+
+        const current = {
+            "type": type, 
+            "content": content,
+            "bDisplay": true,
+            "id": notifications.length
+        };
+    
+        return [...notifications, current];
+    }
+    
+    const hideNotification = (notifications, targetId) =>{
+    
+        const result = [];
+    
+        for(let i = 0; i < notifications.length; i++){
+    
+            const n = notifications[i];
+    
+            if(n.id === targetId) n.bDisplay = false;
+            result.push(n);
+        }
+    
+        return result;
+    }
     
     return [
         addNotification,
