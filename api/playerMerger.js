28cb1e2c7b5df2fa322393439cb32d08539a10d2
Scott Adkin <scottadkin001@gmail.com>
2023-11-21 22:23:38 +0000

    Set up ctf best data

--- api/playerMerger.js
+++ api/playerMerger.js
@@ -32,6 +32,102 @@ class PlayerMerger{
         return await mysql.simpleQuery(query, [this.newId, this.oldId]);
     }
 
+
+    recalCTFBest(data){
+
+        const totals = {};
+
+        const mergeTypes = [
+            "flag_assist",
+            "flag_return",
+            "flag_return_base",
+            "flag_return_mid",
+            "flag_return_enemy_base",
+            "flag_return_save",
+            "flag_dropped",
+            "flag_kill",
+            "flag_suicide",
+            "flag_seal",
+            "flag_seal_pass",
+            "flag_seal_fail",
+            "flag_cover",
+            "flag_cover_pass",
+            "flag_cover_fail",
+            "flag_cover_multi",
+            "flag_cover_spree",
+            "flag_capture",
+            "flag_carry_time",
+            "flag_taken",
+            "flag_pickup",
+            "flag_self_cover",
+            "flag_self_cover_pass",
+            "flag_self_cover_fail",
+            "flag_solo_capture"
+        ];
+
+        const higherBetter = [
+            "best_single_seal",
+            "best_single_cover",
+            "best_single_self_cover"
+        ];
+
+        for(const [gametypeId, gametypeData] of Object.entries(data)){
+
+
+            if(totals[gametypeId] === undefined){
+                totals[gametypeId] = gametypeData;
+                continue;
+            }
+
+            const t = totals[gametypeId];
+
+            for(let i = 0; i < mergeTypes.length; i++){
+
+                const m = mergeTypes[i];
+
+                t[m] += gametypeData[m];
+
+            }
+
+            for(let i = 0; i < higherBetter.length; i++){
+
+                const h = higherBetter[i];
+
+                if(t[h] < gametypeData[h]) t[h] = gametypeData[h];
+            }
+
+            console.log(gametypeId);
+            console.log(`FOUND DUPLICATE DATA`);
+        }
+
+        return totals;
+    }
+
+    async recalcCTFTotals(){
+
+        const tables = [
+            "player_ctf_best",
+            "player_ctf_best_life",
+            "player_ctf_totals"
+        ];
+
+        const data = {};
+
+        for(let i = 0; i < tables.length; i++){
+
+            const t = tables[i];
+
+            const query = `SELECT * FROM nstats_${t} WHERE player_id=?`;
+            data[t] = await mysql.simpleQuery(query, [this.newId]);
+        }
+        
+
+        const best = this.recalCTFBest(data["player_ctf_best"]);
+
+        console.log(best);
+    }
+
+    //This does not include nstats_player_match and totals table
     async mergeCTFTables(){
 
         //ctf_assists
@@ -46,67 +142,67 @@ class PlayerMerger{
             "ctf_assists": {
                 "query": "SET player_id=? WHERE player_id=?", 
                 "vars": [newId, oldId]
-            }, //player_id
+            }, 
             "ctf_caps": {
                 "query": "SET grab_player = IF(grab_player=?,?,grab_player), cap_player = IF(cap_player=?,?,cap_player)", 
                 "vars": [oldId, newId, oldId, newId]
-            }, //grab_player, cap_player,
+            }, 
             "ctf_carry_times": {
                 "query": "SET player_id=? WHERE player_id=?", 
                 "vars": [oldId, newId]
-            }, //player_id
+            }, 
             "ctf_covers": {  
                 "query": "SET killer_id = IF(killer_id=?,?,killer_id), victim_id = IF(victim_id=?,?,victim_id)", 
                 "vars": [oldId, newId, oldId, newId]
-            }, //killer_id, victim_id
+            }, 
             "ctf_cr_kills": {
                 "query": "SET player_id=? WHERE player_id=?", 
                 "vars": [oldId, newId]
-            }, //player_id
+            }, 
             "ctf_events": {
                 "query": "SET player=? WHERE player=?", 
                 "vars": [oldId, newId]
-            }, //player
+            }, 
             "ctf_flag_deaths": {
                 "query": "SET killer_id = IF(killer_id=?,?,killer_id), victim_id = IF(victim_id=?,?,victim_id)", 
                 "vars": [oldId, newId, oldId, newId]
-            }, //killer_id, victim_id,
+            }, 
             "ctf_flag_drops": {
                 "query": "SET player_id=? WHERE player_id=?", 
                 "vars": [oldId, newId]
-            }, //player_id
+            }, 
             "ctf_flag_pickups": {
                 "query": "SET player_id=? WHERE player_id=?", 
                 "vars": [oldId, newId]
-            }, //player_id
+            }, 
             "ctf_returns": {
                 "query": "SET grab_player = IF(grab_player=?,?,grab_player), return_player = IF(return_player=?,?,return_player)", 
                 "vars": [oldId, newId, oldId, newId]
-            }, //grab_player, return_player
+            }, 
             "ctf_seals": {
                 "query": "SET killer_id = IF(killer_id=?,?,killer_id), victim_id = IF(victim_id=?,?,victim_id)", 
                 "vars": [oldId, newId, oldId, newId]
-            }, //killer_id, victim_id
+            }, 
             "ctf_self_covers": {
                 "query": "SET killer_id = IF(killer_id=?,?,killer_id), victim_id = IF(victim_id=?,?,victim_id)", 
                 "vars": [oldId, newId, oldId, newId]
-            }, //killer_id, victim_id
+            }, 
             "player_ctf_best": {
                 "query": "SET player_id=? WHERE player_id=?", 
                 "vars": [oldId, newId]
-            }, //player_id(recalc totals, delete duplicate)
+            }, //(recalc totals, delete duplicate)
             "player_ctf_best_life": {
                 "query": "SET player_id=? WHERE player_id=?", 
                 "vars": [oldId, newId]
-            }, //player_id(recalc totals, delete duplicate)
+            }, //(recalc totals, delete duplicate)
             "player_ctf_match": {
                 "query": "SET player_id=? WHERE player_id=?", 
                 "vars": [oldId, newId]
-            }, //player_id
+            }, 
             "player_ctf_totals": {
                 "query": "SET player_id=? WHERE player_id=?", 
                 "vars": [oldId, newId]
-            }, //player_id (recalc totals, delete duplicate)
+            }, // (recalc totals, delete duplicate)
         };
 
 
@@ -115,6 +211,7 @@ class PlayerMerger{
             await mysql.simpleQuery(`UPDATE nstats_${table} ${info.query}`, info.vars);
         }
     
+        await this.recalcCTFTotals();
         
     }
 }
--- mergetest.js
+++ mergetest.js
@@ -2,9 +2,12 @@ const PlayerMerger = require("./api/playerMerger");
 
 (async () =>{
 
-    const p = new PlayerMerger(2,1337);
+    //const p = new PlayerMerger(2,1337);
+    const p = new PlayerMerger(2,1);
 
-    await p.merge();
+    //await p.merge();
+
+    await p.recalcCTFTotals();
 
     process.exit();
 })();
