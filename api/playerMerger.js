494f9a5e476260bb3b29127c45a5bf9f3f9a477d
Scott Adkin <scottadkin001@gmail.com>
2023-11-22 16:42:02 +0000

    Fixed player ids in wrong order, merge ctf player data

--- api/playerMerger.js
+++ api/playerMerger.js
@@ -1,4 +1,5 @@
 const mysql = require("./database");
+const Message = require("./message");
 
 /**
  * Maybe this time I get it right...
@@ -172,17 +173,107 @@ class PlayerMerger{
             "player_ctf_totals"
         ];
 
+        
         for(let i = 0; i < tables.length; i++){
 
             const t = tables[i];
 
+            new Message(`Delete old data from table ${t}.`,"note");
+
             await mysql.simpleQuery(`DELETE FROM nstats_${t} WHERE player_id=?`, [this.newId]);
+            new Message(`Deleted old data from table ${t}.`,"pass");
         }
     }
 
     async insertNewCTFData(totals, best, bestLife){
 
-        //delete newId and insert new data
+
+        const totalsQuery = `INSERT INTO nstats_player_ctf_totals (
+            player_id, gametype_id, total_matches, playtime, flag_assist,
+            flag_return, flag_return_base, flag_return_mid, flag_return_enemy_base, flag_return_save,
+            flag_dropped, flag_kill, flag_suicide, flag_seal, flag_seal_pass, 
+            flag_seal_fail, best_single_seal, flag_cover, flag_cover_pass, flag_cover_fail,
+            flag_cover_multi, flag_cover_spree, best_single_cover, flag_capture, flag_carry_time,
+            flag_taken, flag_pickup, flag_self_cover, flag_self_cover_pass, flag_self_cover_fail,
+            best_single_self_cover, flag_solo_capture) VALUES ?`;
+
+        const totalInsertVars = [];
+
+        for(const t of Object.values(totals)){
+
+            totalInsertVars.push([
+                t.player_id, t.gametype_id, t.total_matches, t.playtime, t.flag_assist,
+                t.flag_return, t.flag_return_base, t.flag_return_mid, t.flag_return_enemy_base, t.flag_return_save,
+                t.flag_dropped, t.flag_kill, t.flag_suicide, t.flag_seal, t.flag_seal_pass, 
+                t.flag_seal_fail, t.best_single_seal, t.flag_cover, t.flag_cover_pass, t.flag_cover_fail,
+                t.flag_cover_multi, t.flag_cover_spree, t.best_single_cover, t.flag_capture, t.flag_carry_time,
+                t.flag_taken, t.flag_pickup, t.flag_self_cover, t.flag_self_cover_pass, t.flag_self_cover_fail,
+                t.best_single_self_cover, t.flag_solo_capture
+            ]);
+        }
+
+        new Message(`Start inserting new player ctf totals(${totalInsertVars.length} rows).`,"note");
+        await mysql.bulkInsert(totalsQuery, totalInsertVars);
+        new Message(`Finished inserting new player ctf totals.`,"pass");
+
+        const bestQuery = `INSERT INTO nstats_player_ctf_best (
+            player_id, gametype_id, flag_assist, flag_return, flag_return_base,
+            flag_return_mid, flag_return_enemy_base, flag_return_save, flag_dropped, flag_kill,
+            flag_suicide, flag_seal, flag_seal_pass, flag_seal_fail, best_single_seal,
+            flag_cover, flag_cover_pass, flag_cover_fail, flag_cover_multi, flag_cover_spree,
+            best_single_cover, flag_capture, flag_carry_time, flag_taken, flag_pickup,
+            flag_self_cover, flag_self_cover_pass, flag_self_cover_fail, best_single_self_cover, flag_solo_capture
+        ) VALUES ?`;
+
+
+        const bestInsertVars = [];
+
+        for(const b of Object.values(best)){
+
+            bestInsertVars.push([
+                b.player_id, b.gametype_id, b.flag_assist, b.flag_return, b.flag_return_base,
+                b.flag_return_mid, b.flag_return_enemy_base, b.flag_return_save, b.flag_dropped, b.flag_kill,
+                b.flag_suicide, b.flag_seal, b.flag_seal_pass, b.flag_seal_fail, b.best_single_seal,
+                b.flag_cover, b.flag_cover_pass, b.flag_cover_fail, b.flag_cover_multi, b.flag_cover_spree,
+                b.best_single_cover, b.flag_capture, b.flag_carry_time, b.flag_taken, b.flag_pickup,
+                b.flag_self_cover, b.flag_self_cover_pass, b.flag_self_cover_fail, b.best_single_self_cover, b.flag_solo_capture
+            ]);
+        }
+
+        new Message(`Start inserting new player ctf best values(${bestInsertVars.length} rows).`,"note");
+        await mysql.bulkInsert(bestQuery, bestInsertVars);
+        new Message(`Finished inserting new player ctf best values.`,"pass");
+
+
+        const bestLifeQuery = `INSERT INTO nstats_player_ctf_best_life (
+            player_id, gametype_id, flag_assist, flag_return, flag_return_base,
+            flag_return_mid, flag_return_enemy_base, flag_return_save, flag_dropped, flag_kill,
+            flag_seal, flag_seal_pass, flag_seal_fail, best_single_seal,
+            flag_cover, flag_cover_pass, flag_cover_fail, flag_cover_multi, flag_cover_spree,
+            best_single_cover, flag_capture, flag_carry_time, flag_taken, flag_pickup,
+            flag_self_cover, flag_self_cover_pass, flag_self_cover_fail, best_single_self_cover, flag_solo_capture
+        ) VALUES ?`;
+
+
+        const bestLifeInsertVars = [];
+
+        for(const b of Object.values(bestLife)){
+
+            bestLifeInsertVars.push([
+                b.player_id, b.gametype_id, b.flag_assist, b.flag_return, b.flag_return_base,
+                b.flag_return_mid, b.flag_return_enemy_base, b.flag_return_save, b.flag_dropped, b.flag_kill,
+                b.flag_seal, b.flag_seal_pass, b.flag_seal_fail, b.best_single_seal,
+                b.flag_cover, b.flag_cover_pass, b.flag_cover_fail, b.flag_cover_multi, b.flag_cover_spree,
+                b.best_single_cover, b.flag_capture, b.flag_carry_time, b.flag_taken, b.flag_pickup,
+                b.flag_self_cover, b.flag_self_cover_pass, b.flag_self_cover_fail, b.best_single_self_cover, b.flag_solo_capture
+            ]);
+        }
+
+        new Message(`Start inserting new player ctf best life values(${bestLifeInsertVars.length} rows).`,"note");
+        await mysql.bulkInsert(bestLifeQuery, bestLifeInsertVars);
+        new Message(`Finished inserting new player ctf best life values.`,"pass");
+
+
     }
 
     async recalCTFTotals(){
@@ -211,7 +302,9 @@ class PlayerMerger{
         const bestLife = this.recalCTFBest(data["player_ctf_best_life"]);
         const totals = this.recalCTFPlayerTotals(data["player_ctf_totals"]);
 
-        //await this.deleteOldCTFData();
+        await this.deleteOldCTFData();
+
+        await this.insertNewCTFData(totals, best, bestLife);
 
         //console.log(best);
         //console.log(bestLife);
@@ -239,7 +332,7 @@ class PlayerMerger{
             }, 
             "ctf_carry_times": {
                 "query": "SET player_id=? WHERE player_id=?", 
-                "vars": [oldId, newId]
+                "vars": [newId, oldId]
             }, 
             "ctf_covers": {  
                 "query": "SET killer_id = IF(killer_id=?,?,killer_id), victim_id = IF(victim_id=?,?,victim_id)", 
@@ -247,11 +340,11 @@ class PlayerMerger{
             }, 
             "ctf_cr_kills": {
                 "query": "SET player_id=? WHERE player_id=?", 
-                "vars": [oldId, newId]
+                "vars": [newId, oldId]
             }, 
             "ctf_events": {
                 "query": "SET player=? WHERE player=?", 
-                "vars": [oldId, newId]
+                "vars": [newId, oldId]
             }, 
             "ctf_flag_deaths": {
                 "query": "SET killer_id = IF(killer_id=?,?,killer_id), victim_id = IF(victim_id=?,?,victim_id)", 
@@ -259,11 +352,11 @@ class PlayerMerger{
             }, 
             "ctf_flag_drops": {
                 "query": "SET player_id=? WHERE player_id=?", 
-                "vars": [oldId, newId]
+                "vars": [newId, oldId]
             }, 
             "ctf_flag_pickups": {
                 "query": "SET player_id=? WHERE player_id=?", 
-                "vars": [oldId, newId]
+                "vars": [newId, oldId]
             }, 
             "ctf_returns": {
                 "query": "SET grab_player = IF(grab_player=?,?,grab_player), return_player = IF(return_player=?,?,return_player)", 
@@ -279,19 +372,19 @@ class PlayerMerger{
             }, 
             "player_ctf_best": {
                 "query": "SET player_id=? WHERE player_id=?", 
-                "vars": [oldId, newId]
+                "vars": [newId, oldId]
             }, //(recalc totals, delete duplicate)
             "player_ctf_best_life": {
                 "query": "SET player_id=? WHERE player_id=?", 
-                "vars": [oldId, newId]
+                "vars": [newId, oldId]
             }, //(recalc totals, delete duplicate)
             "player_ctf_match": {
                 "query": "SET player_id=? WHERE player_id=?", 
-                "vars": [oldId, newId]
+                "vars": [newId, oldId]
             }, 
             "player_ctf_totals": {
                 "query": "SET player_id=? WHERE player_id=?", 
-                "vars": [oldId, newId]
+                "vars": [newId, oldId]
             }, // (recalc totals, delete duplicate)
         };
 
--- mergetest.js
+++ mergetest.js
@@ -3,11 +3,11 @@ const PlayerMerger = require("./api/playerMerger");
 (async () =>{
 
     //const p = new PlayerMerger(2,1337);
-    const p = new PlayerMerger(2,1);
+    const p = new PlayerMerger(537,571);
 
-    //await p.merge();
+    await p.merge();
 
-    await p.recalCTFTotals();
+    //await p.recalCTFTotals();
 
     process.exit();
 })();
