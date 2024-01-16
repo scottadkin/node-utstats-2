2d882add5b7c4454700613a0c3b129422e8ab300
Scott Adkin <scottadkin001@gmail.com>
2024-01-16 17:55:35 +0000

    Refactoring, set up tabs for new types

--- components/MatchWeaponSummaryCharts/index.js
+++ components/MatchWeaponSummaryCharts/index.js
@@ -1,4 +1,4 @@
-import {React, useEffect, useState} from "react";
+import {React, useEffect, useState, useReducer} from "react";
 import BarChart from "../BarChart";
 import InteractiveTable from "../InteractiveTable";
 import CountryFlag from "../CountryFlag";
@@ -8,6 +8,85 @@ import Loading from "../Loading";
 import MatchWeaponBest from "../MatchWeaponBest";
 import Tabs from "../Tabs";
 import { ignore0, getPlayer, getTeamColor } from "../../api/generic.mjs";
+
+const reducer = (state, action) =>{
+
+    switch(action.type){
+
+        case "set-loading":{
+            return {
+                ...state,
+                "bLoading": action.value
+            }
+        }
+
+        case "set-mode": {
+            return {
+                ...state,
+                "mode": action.value
+            }
+        }
+
+        case "set-weapon-stats": {
+          
+            return {
+                ...state,
+                "weaponStats": {"names": action.value.names, "playerData": action.value.playerData}
+            }
+        }
+
+        case "set-individual-mode": {
+            console.log("CHECK");
+            return {
+                ...state,
+                "individualDisplayMode": action.value
+            }
+        }
+
+        case "set-selected-weapon": {
+            return {
+                ...state,
+                "selectedWeaponId": action.value
+            }
+        }
+
+        case "set-stats-type":{
+            return {
+                ...state,
+                "selectedStatType": action.value
+            }
+        }
+
+    }
+
+    return state;
+}
+
+const orderByName = (a, b) =>{
+
+    a = a.name.value;
+    b = b.name.value;
+
+    if(a < b) return -1;
+    if(a > b) return 1;
+    return 0;
+}
+
+const renderTabs = (state, dispatch) =>{
+
+    return <Tabs options={[
+        {"name": "Total Damage", "value": -2},
+        {"name": "Best Stats", "value": -1},
+        {"name": "Individual Weapons", "value": 0},
+        {"name": "By Stats Type", "value": -3}
+       // {"name": "Bar Charts", "value": 1},
+    ]} 
+        selectedValue={state.mode}
+        changeSelected={(value) =>{
+            dispatch({"type": "set-mode", "value": value})
+        }}
+    />
+}
 
 const getMaxStats = (weaponStats, weaponId, type) =>{
 
@@ -31,13 +110,13 @@ const getMaxStats = (weaponStats, weaponId, type) =>{
     return best;
 }
 
-const renderBest = (mode, matchId, weaponStats, players, totalTeams) =>{
+const renderBest = (state, matchId, players, totalTeams) =>{
 
-    if(mode !== -1) return null;
+    if(state.mode !== -1) return null;
 
     const elems = [];
 
-    weaponStats.names.sort((a, b) =>{
+    state.weaponStats.names.sort((a, b) =>{
 
         a = a.name.toLowerCase();
         b = b.name.toLowerCase();
@@ -47,14 +126,14 @@ const renderBest = (mode, matchId, weaponStats, players, totalTeams) =>{
         return 0;
     });
 
-    for(let i = 0; i < weaponStats.names.length; i++){
+    for(let i = 0; i < state.weaponStats.names.length; i++){
 
-        const {id, name} = weaponStats.names[i];
+        const {id, name} = state.weaponStats.names[i];
 
-        const bestKills = getMaxStats(weaponStats.playerData, id, "kills");
+        const bestKills = getMaxStats(state.weaponStats.playerData, id, "kills");
         const bestKillsPlayer = getPlayer(players, bestKills.player_id, true);
 
-        const bestDamage = getMaxStats(weaponStats.playerData, id, "damage");
+        const bestDamage = getMaxStats(state.weaponStats.playerData, id, "damage");
         const bestDamagePlayer = getPlayer(players, bestDamage.player_id, true);
 
         elems.push(<MatchWeaponBest 
@@ -82,9 +161,9 @@ const renderBest = (mode, matchId, weaponStats, players, totalTeams) =>{
     </div>
 }
 
-const renderIndividualTabs = (mode, individualDisplayMode, setIndividualDisplayMode) =>{
+const renderIndividualTabs = (state, dispatch) =>{
    
-    if(mode !== 0) return null;
+    if(state.mode !== 0) return null;
 
     const options = [
         {
@@ -97,7 +176,10 @@ const renderIndividualTabs = (mode, individualDisplayMode, setIndividualDisplayM
         }
     ];
 
-    return <Tabs options={options} selectedValue={individualDisplayMode} changeSelected={setIndividualDisplayMode}/>
+    return <Tabs options={options} selectedValue={state.individualDisplayMode} changeSelected={(value) =>{
+        console.log(value);
+        dispatch({"type": "set-individual-mode", "value": value});
+    }}/>
 }
 
 const createPlayerTotalStats = (data, players) =>{
@@ -201,16 +283,166 @@ const renderTotalDamage = (displayMode, matchId, totalData, players, totalTeams)
     </>
 }
 
+const renderByStatsType = (state, dispatch) =>{
+
+    if(state.mode !== -3) return null;
+
+    return <>
+        <Tabs 
+            options={[
+                {"value": 0, "name": "Kills"},
+                {"value": 1, "name": "Deaths"},
+                {"value": 2, "name": "Damage"},
+                {"value": 3, "name": "Accuracy"},
+                {"value": 4, "name": "Hits"},
+                {"value": 5, "name": "Shots"}
+            ]}
+            selectedValue={state.statsType}
+            changeSelected={(value) =>{
+                dispatch({"type": "set-stats-type", "value": value});
+            }}
+        />
+    </>
+}
+
+
+const renderSingleTable = (state, totalTeams, matchId, playerData) =>{
+
+    if(state.mode !== 0 || state.individualDisplayMode !== 0) return null;
+    
+    const headers = {
+        "name": "Player",
+        "shots": "Shots",
+        "hits": "Hits",
+        "accuracy": "Accuracy",
+        "kills": "Kills",
+        "bestKills": {"title": "Best Spree", "content": "Most kills with a weapon in a single life."},
+        "teamKills": "Team Kills",
+        "deaths": "Deaths",  
+        "suicides": "Suicides",
+        "eff": "Efficiency",
+        "damage": "Damage"
+    };
+
+    const weaponName = getWeaponName(state.weaponStats, state.selectedWeaponId);
+
+    const data = [];
+
+    for(let i = 0; i < state.weaponStats.playerData.length; i++){
+
+        const d = state.weaponStats.playerData[i];
+
+        if(d.weapon_id !== state.selectedWeaponId) continue;
+
+        const player = getPlayer(playerData, d.player_id, true);
+
+        data.push({
+            "name": {
+                "value": player.name.toLowerCase(), 
+                "className": `text-left ${getTeamColor(player.team, totalTeams)}`,
+                "displayValue": <Link href={`/pmatch/${matchId}/?player=${d.player_id}`}>
+                    
+                    <CountryFlag country={player.country}/>
+                    {player.name} 
+                    
+                </Link>
+            },
+            "shots": {"value": d.shots, "displayValue": ignore0(d.shots)},
+            "hits": {"value": d.hits, "displayValue": ignore0(d.hits)},
+            "accuracy": {"value": d.accuracy, "displayValue": `${d.accuracy.toFixed(2)}%`},
+            "deaths": {"value": d.deaths, "displayValue": ignore0(d.deaths)},
+            "suicides": {"value": d.suicides, "displayValue": ignore0(d.suicides)},
+            "kills": {"value": d.kills, "displayValue": ignore0(d.kills)},
+            "bestKills": {"value": d.best_kills, "displayValue": ignore0(d.best_kills)},
+            "teamKills": {"value": d.team_kills, "displayValue": ignore0(d.team_kills)},
+            "damage": {"value": d.damage, "displayValue": ignore0(d.damage)},
+            "eff": {"value": d.efficiency, "displayValue": `${d.efficiency.toFixed(2)}%`}
+        });
+    }
+
+    data.sort(orderByName);
+
+    return <InteractiveTable key={state.selectedWeaponId} width="1" title={weaponName} headers={headers} data={data}/>
+}
+
+const getWeaponName = (weaponStats, id) =>{
+
+    //console.log(weaponStats, id);
+
+    for(let i = 0; i < weaponStats.names.length; i++){
+
+        const w = weaponStats.names[i]
+
+        if(w.id === id) return w.name;        
+    }
+
+    return "Not Found";
+}
+
+const renderBarChart = (state, dispatch, playerData) =>{
+
+    if(state.mode !== 0 || state.individualDisplayMode !== 1) return null;
+
+    const weaponName = getWeaponName(state.weaponStats, state.selectedWeaponId);
+
+    const values = [];
+    const names = [];
+
+    for(const [playerId, data] of Object.entries(playerData)){
+        names.push(data.name);
+        values.push(getPlayerWeaponStat(state, parseInt(playerId)));
+    }
+
+
+    return <>
+        <Tabs options={[
+                {"name": "Kills", "value": "kills"},
+                {"name": "Deaths", "value": "deaths"},
+                {"name": "Damage", "value": "damage"},
+                {"name": "Shots", "value": "shots"},
+                {"name": "Hits", "value": "hits"},
+                {"name": "Accuracy", "value": "accuracy"},
+            ]}
+            selectedValue={state.selectedStatType}
+            changeSelected={(value) => {dispatch({"type": "set-stats-type", "value": value})}}
+        />
+        <BarChart title={weaponName} label={state.selectedStatType.toUpperCase()} values={values} names={names}/>
+    </>
+}
+
+const getPlayerWeaponStat = (state, playerId) =>{
+
+    for(let i = 0; i < state.weaponStats.playerData.length; i++){
+
+        const p = state.weaponStats.playerData[i];
+
+        if(p.player_id === playerId && state.selectedWeaponId === p.weapon_id){
+
+            let value = p[state.selectedStatType];
+
+            if(state.selectedStatType === "accuracy") value = parseFloat(value.toFixed(2));
+            return value;
+        }
+    }
+
+    return 0;
+}
+
+
 const MatchWeaponSummaryCharts = ({matchId, totalTeams, playerData, host}) =>{
 
-    const [bLoading, setbLoading] = useState(true);
     const [error, setError] = useState(null);
-    const [weaponStats, setWeaponStats] = useState({"names": [], "playerData": []});
-    const [displayMode, setDisplayMode] = useState(0);
-    const [selectedWeaponId, setSelectedWeaponId] = useState(null);
-    const [selectedStatType, setSelectedStatType] = useState("kills");
-    const [individualDisplayMode, setIndividualDisplayMode] = useState(0);
     const [totalStats, setTotalStats] = useState([]);
+
+    const [state, dispatch] = useReducer(reducer, {
+        "bLoading": true,
+        "mode": -3,
+        //"statsType": 0,
+        "individualDisplayMode": 0,
+        "selectedStatType": "kills",
+        "weaponStats": {"names": [], "playerData": []},
+        "selectedWeaponId": null
+    });
 
 
     useEffect(() =>{
@@ -238,20 +470,21 @@ const MatchWeaponSummaryCharts = ({matchId, totalTeams, playerData, host}) =>{
                     setError(null);
 
                     if(res.names.length > 0){
-                        setSelectedWeaponId(res.names[0].id)
+                        dispatch({"type": "set-selected-weapon", "value": res.names[0].id});
                     }
 
-                    setWeaponStats(res);
+                    dispatch({"type": "set-weapon-stats", "value": res});
 
                     setTotalStats(createPlayerTotalStats(res.playerData, playerData));
                 }
 
 
             }catch(err){
+                if(err.name !== "AbortError")
                 setError(err.toString());
             }
 
-            setbLoading(false);
+            dispatch({"type": "set-loading", "value": false});
         }
 
 
@@ -263,25 +496,13 @@ const MatchWeaponSummaryCharts = ({matchId, totalTeams, playerData, host}) =>{
 
     }, [matchId, playerData]);
 
-    const renderTabs = () =>{
 
-        return <Tabs options={[
-            {"name": "Total Damage", "value": -2},
-            {"name": "Best Stats", "value": -1},
-            {"name": "Individual Weapons", "value": 0},
-           // {"name": "Bar Charts", "value": 1},
-        ]} 
-            selectedValue={displayMode}
-            changeSelected={setDisplayMode}
-        />
-    }
+    const renderWeaponTabs = (state) =>{
 
-    const renderWeaponTabs = () =>{
-
-        if(displayMode < 0) return null;
+        if(state.mode < 0) return null;
         const tabs = [];
 
-        const names = [...weaponStats.names];
+        const names = [...state.weaponStats.names];
 
         names.sort((a, b) =>{
 
@@ -297,9 +518,9 @@ const MatchWeaponSummaryCharts = ({matchId, totalTeams, playerData, host}) =>{
 
             const weapon = names[i];
 
-            const styleClass = `tab ${(selectedWeaponId === weapon.id) ?  "tab-selected": ""}`;
+            const styleClass = `tab ${(state.selectedWeaponId === weapon.id) ?  "tab-selected": ""}`;
 
-            tabs.push(<div key={weapon.id} className={styleClass} onClick={() => setSelectedWeaponId(weapon.id)}>
+            tabs.push(<div key={weapon.id} className={styleClass} onClick={() => dispatch({"type": "set-selected-weapon", "value": weapon.id})}>
                 {weapon.name}
             </div>);
         }
@@ -311,158 +532,31 @@ const MatchWeaponSummaryCharts = ({matchId, totalTeams, playerData, host}) =>{
         </div>
     }
 
-    const getWeaponName = (id) =>{
+    
 
-        for(let i = 0; i < weaponStats.names.length; i++){
-
-            const w = weaponStats.names[i]
-
-            if(w.id === id) return w.name;        
-        }
-
-        return "Not Found";
-    }
-
-    const orderByName = (a, b) =>{
-
-        a = a.name.value;
-        b = b.name.value;
-
-        if(a < b) return -1;
-        if(a > b) return 1;
-        return 0;
-    }
-
-    const renderSingleTable = () =>{
-
-        if(displayMode !== 0 || individualDisplayMode !== 0) return null;
-        
-        const headers = {
-            "name": "Player",
-            "shots": "Shots",
-            "hits": "Hits",
-            "accuracy": "Accuracy",
-            "kills": "Kills",
-            "bestKills": {"title": "Best Spree", "content": "Most kills with a weapon in a single life."},
-            "teamKills": "Team Kills",
-            "deaths": "Deaths",  
-            "suicides": "Suicides",
-            "eff": "Efficiency",
-            "damage": "Damage"
-        };
-
-        const weaponName = getWeaponName(selectedWeaponId);
-
-        const data = [];
-
-        for(let i = 0; i < weaponStats.playerData.length; i++){
-
-            const d = weaponStats.playerData[i];
-
-            if(d.weapon_id !== selectedWeaponId) continue;
-
-            const player = getPlayer(playerData, d.player_id, true);
-
-            data.push({
-                "name": {
-                    "value": player.name.toLowerCase(), 
-                    "className": `text-left ${getTeamColor(player.team, totalTeams)}`,
-                    "displayValue": <Link href={`/pmatch/${matchId}/?player=${d.player_id}`}>
-                        
-                        <CountryFlag country={player.country}/>
-                        {player.name} 
-                        
-                    </Link>
-                },
-                "shots": {"value": d.shots, "displayValue": ignore0(d.shots)},
-                "hits": {"value": d.hits, "displayValue": ignore0(d.hits)},
-                "accuracy": {"value": d.accuracy, "displayValue": `${d.accuracy.toFixed(2)}%`},
-                "deaths": {"value": d.deaths, "displayValue": ignore0(d.deaths)},
-                "suicides": {"value": d.suicides, "displayValue": ignore0(d.suicides)},
-                "kills": {"value": d.kills, "displayValue": ignore0(d.kills)},
-                "bestKills": {"value": d.best_kills, "displayValue": ignore0(d.best_kills)},
-                "teamKills": {"value": d.team_kills, "displayValue": ignore0(d.team_kills)},
-                "damage": {"value": d.damage, "displayValue": ignore0(d.damage)},
-                "eff": {"value": d.efficiency, "displayValue": `${d.efficiency.toFixed(2)}%`}
-            });
-        }
-
-        data.sort(orderByName);
-
-        return <InteractiveTable key={selectedWeaponId} width="1" title={weaponName} headers={headers} data={data}/>
-    }
     
 
     if(error !== null){
         return <ErrorMessage title="Weapon Statistics" text={error}/>
     }
 
-    if(bLoading){
+    if(state.bLoading){
         return <Loading />;
     }   
 
 
-    const getPlayerWeaponStat = (playerId) =>{
-
-        for(let i = 0; i < weaponStats.playerData.length; i++){
-
-            const p = weaponStats.playerData[i];
-
-            if(p.player_id === playerId && selectedWeaponId === p.weapon_id){
-
-                let value = p[selectedStatType];
-
-                if(selectedStatType === "accuracy") value = parseFloat(value.toFixed(2));
-                return value;
-            }
-        }
-
-        return 0;
-    }
-
-    const renderBarChart = () =>{
-
-        if(displayMode !== 0 || individualDisplayMode !== 1) return null;
-
-        const weaponName = getWeaponName(selectedWeaponId);
-
-        const values = [];
-        const names = [];
-
-        for(const [playerId, data] of Object.entries(playerData)){
-            names.push(data.name);
-            values.push(getPlayerWeaponStat(parseInt(playerId)));
-        }
-
-
-        return <div>
-            <div className="tabs">
-                <div className={`tab ${(selectedStatType === "kills") ? "tab-selected" : ""}`} 
-                    onClick={() => setSelectedStatType("kills")}>Kills</div>
-                <div className={`tab ${(selectedStatType === "deaths") ? "tab-selected" : ""}`} 
-                    onClick={() => setSelectedStatType("deaths")}>Deaths</div>
-                <div className={`tab ${(selectedStatType === "damage") ? "tab-selected" : ""}`} 
-                    onClick={() => setSelectedStatType("damage")}>Damage</div>
-                <div className={`tab ${(selectedStatType === "shots") ? "tab-selected" : ""}`} 
-                    onClick={() => setSelectedStatType("shots")}>Shots</div>
-                <div className={`tab ${(selectedStatType === "hits") ? "tab-selected" : ""}`} 
-                    onClick={() => setSelectedStatType("hits")}>Hits</div>
-                <div className={`tab ${(selectedStatType === "accuracy") ? "tab-selected" : ""}`} 
-                    onClick={() => setSelectedStatType("accuracy")}>Accuracy</div>
-            </div>
-            <BarChart title={weaponName} label={selectedStatType.toUpperCase()} values={values} names={names}/>
-        </div>
-    }
+    
 
     return <div>
         <div className="default-header">Weapon Statistics</div>
-        {renderTabs()}
-        {renderIndividualTabs(displayMode, individualDisplayMode, setIndividualDisplayMode)}
-        {renderWeaponTabs()}
-        {renderTotalDamage(displayMode, matchId, totalStats, playerData, totalTeams)}
-        {renderBest(displayMode, matchId, weaponStats, playerData, totalTeams)}
-        {renderBarChart()}
-        {renderSingleTable()}
+        {renderTabs(state, dispatch)}
+        {renderIndividualTabs(state, dispatch)}
+        {renderWeaponTabs(state)}
+        {renderTotalDamage(state.mode, matchId, totalStats, playerData, totalTeams)}
+        {renderBest(state, matchId, playerData, totalTeams)}
+        {renderBarChart(state, dispatch, playerData)}
+        {renderSingleTable(state, totalTeams, matchId, playerData)}
+        {renderByStatsType(state, dispatch)}
     </div>
 }
 
