8e6262d3c3a1a9d402ba0190bd7f675dfb7baecd
Scott Adkin <scottadkin001@gmail.com>
2024-03-12 16:00:36 +0000

    Button component for pagination

--- src/app/UI/Pagination.js
+++ src/app/UI/Pagination.js
@@ -6,8 +6,14 @@ function Button({url, targetPage, currentPage}){
 
     //check if targetPage === current page and highlight if true
 
+    let className = styles.button;
+
+    if(targetPage === currentPage){
+        className = `${styles.button} ${styles.active}`;
+    }
+
     return <Link key={targetPage} href={`${url}${targetPage}`}>
-        <div className={styles.button}>{targetPage}</div>
+        <div className={className}>{targetPage}</div>
     </Link>
 }
 
@@ -25,47 +31,43 @@ export default function Pagination({url, results, currentPage, perPage}){
     if(perPage !== perPage) perPage = 25;
     if(perPage < 1) perPage = 1;
 
-    let totalPages = Math.floor(results / perPage);
+    let totalPages = Math.ceil(results / perPage);
 
     const nextPage = currentPage + 1;
     const previousPage = currentPage - 1;
 
+    let start = perPage * (currentPage - 1) + 1;
+    let end = (start + perPage > results) ? results : start + perPage - 1;
 
     const buttons = [];
 
     if(totalPages > 0){
-        buttons.push(<Link key={"1"} href={`${url}1`}>
-            <div className={styles.button}>1</div>
-        </Link>);   
+        buttons.push(<Button key="1" url={url} targetPage={1} currentPage={currentPage}/>);
     }
 
     if(previousPage > 1){
-
-        buttons.push(<Link key={"prev"} href={`${url}${previousPage}`}>
-            <div className={styles.button}>{previousPage}</div>
-        </Link>);  
+        buttons.push(<Button key="prev" url={url} targetPage={previousPage} currentPage={currentPage}/>);
     }
 
     if(currentPage > 1 && currentPage <= totalPages){
-        buttons.push(<div key="current" className={`${styles.button} ${styles.active}`}>{currentPage}</div>);
+        buttons.push(<Button key="current" url={url} targetPage={currentPage} currentPage={currentPage}/>);
     }
 
     if(nextPage < totalPages){
-
-        buttons.push(<Link key={"next"} href={`${url}${nextPage}`}>
-            <div className={styles.button}>{nextPage}</div>
-        </Link>);  
+        buttons.push(<Button key="current" url={url} targetPage={nextPage} currentPage={currentPage}/>);
     }
 
     if(totalPages > 1 && nextPage <= totalPages){
-        buttons.push(<Link key={"last"} href={`${url}${totalPages}`}>
-            <div className={styles.button}>{totalPages}</div>
-        </Link>);  
+        buttons.push(<Button key="last" url={url} targetPage={totalPages} currentPage={currentPage}/>);
     }
 
     return <div className={styles.wrapper}>
 
-        Displaying page {currentPage} out of {totalPages}<br/>
+        <div className={styles.info}>
+            Displaying page {currentPage} out of {totalPages}<br/>
+            Results {start} to {end}<br/>
+            Total Results {results}
+        </div>
         {buttons}
 
     </div>
