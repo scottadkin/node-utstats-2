2f29598a5157f0832c079c3ea457ffdca695db11
Scott Adkin <scottadkin001@gmail.com>
2025-09-23 15:46:00 +0100

    Order options ASC

--- src/app/UI/Matches/SearchForm.js
+++ src/app/UI/Matches/SearchForm.js
@@ -15,6 +15,26 @@ function getPerPageOptions(){
     ];
 }
 
+
+function toOrderedArray(data){
+
+    const options = [];
+
+    for(const [id, name] of Object.entries(data)){
+        options.push({id, name});
+    }
+
+    options.sort((a,b) =>{
+        a = a.name.toLowerCase();
+        b = b.name.toLowerCase();
+        if(a < b) return -1;
+        if(a > b) return 1;
+        return 0;
+    });
+
+    return options;
+}
+
 export default function SearchForm({selectedServer, selectedGametype, 
     selectedMap, displayMode, serverNames, gametypeNames, mapNames,
     perPage
@@ -27,7 +47,10 @@ export default function SearchForm({selectedServer, selectedGametype,
         <option key="0" value="0">Any</option>
     ];
 
-    for(const [id, name] of Object.entries(serverNames)){
+    const sOptions = toOrderedArray(serverNames);
+
+    for(let i = 0; i < sOptions.length; i++){
+        const {id, name} = sOptions[i]; 
         serverOptions.push(<option key={id} value={id}>{name}</option>);
     }
 
@@ -35,7 +58,11 @@ export default function SearchForm({selectedServer, selectedGametype,
         <option key="0" value="0">Any</option>
     ];
 
-    for(const [id, name] of Object.entries(gametypeNames)){
+
+    const gOptions = toOrderedArray(gametypeNames);
+
+    for(let i = 0; i < gOptions.length; i++){
+        const {id, name} = gOptions[i]; 
         gametypeOptions.push(<option key={id} value={id}>{name}</option>);
     }
 
@@ -43,7 +70,10 @@ export default function SearchForm({selectedServer, selectedGametype,
         <option key="0" value="0">Any</option>
     ];
 
-    for(const [id, name] of Object.entries(mapNames)){
+    const mOptions = toOrderedArray(mapNames);
+
+    for(let i = 0; i < mOptions.length; i++){
+        const {id, name} = mOptions[i]; 
         mapOptions.push(<option key={id} value={id}>{removeUnr(name)}</option>);
     }
    
@@ -51,7 +81,7 @@ export default function SearchForm({selectedServer, selectedGametype,
     return <div className="form m-bottom-25">
         <div className="form-row">
             <label htmlFor="server">Server</label>
-            <select className="default-select" value={selectedServer} onChange={(e) =>{
+            <select id="server" className="default-select" value={selectedServer} onChange={(e) =>{
                 router.push(`/matches/?server=${e.target.value}&gametype=${selectedGametype}&map=${selectedMap}&displayMode=${displayMode}&pp=${perPage}`);
             }}>
                {serverOptions}
@@ -59,7 +89,7 @@ export default function SearchForm({selectedServer, selectedGametype,
         </div>
         <div className="form-row">
             <label htmlFor="gametype">Gametype</label>
-            <select className="default-select" value={selectedGametype} onChange={(e) =>{
+            <select id="gametype" className="default-select" value={selectedGametype} onChange={(e) =>{
                 router.push(`/matches/?server=${selectedServer}&gametype=${e.target.value}&map=${selectedMap}&displayMode=${displayMode}&pp=${perPage}`);
             }}>
                {gametypeOptions}
@@ -67,7 +97,7 @@ export default function SearchForm({selectedServer, selectedGametype,
         </div>
         <div className="form-row">
             <label htmlFor="map">Map</label>
-            <select className="default-select" value={selectedMap} onChange={(e) =>{
+            <select id="map" className="default-select" value={selectedMap} onChange={(e) =>{
                 router.push(`/matches/?server=${selectedServer}&gametype=${selectedGametype}&map=${e.target.value}&displayMode=${displayMode}&pp=${perPage}`);
             }}>
                {mapOptions}
@@ -75,8 +105,8 @@ export default function SearchForm({selectedServer, selectedGametype,
         </div>
 
         <div className="form-row">
-            <label htmlFor="map">Results Per Page</label>
-            <select className="default-select" value={perPage} onChange={(e) =>{
+            <label htmlFor="pp">Results Per Page</label>
+            <select id="pp" className="default-select" value={perPage} onChange={(e) =>{
                 router.push(`/matches/?server=${selectedServer}&gametype=${selectedGametype}&map=${selectedMap}&displayMode=${displayMode}&pp=${e.target.value}`);
             }}>
                {getPerPageOptions()}
@@ -84,8 +114,8 @@ export default function SearchForm({selectedServer, selectedGametype,
         </div>
 
         <div className="form-row">
-            <label htmlFor="map">Display Mode</label>
-            <select className="default-select" value={displayMode} onChange={(e) =>{
+            <label htmlFor="display">Display Mode</label>
+            <select id="display" className="default-select" value={displayMode} onChange={(e) =>{
                 router.push(`/matches/?server=${selectedServer}&gametype=${selectedGametype}&map=${selectedMap}&displayMode=${e.target.value}&pp=${perPage}`);
             }}>
                <option value="default">Default</option>
